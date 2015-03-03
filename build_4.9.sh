644828ec9f3c7a316c11bd01887c42d8cc55c0c7
wzhy90 <wzhy90@gmail.com>
2015-03-03 18:50:22 +0800

    Delete GCC 4.6 build script

--- build_4.9.sh
+++ build_4.9.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 ### select toolchain
 ## GCC 4.6 Linaro
-#CUR_TC=/home/wzhy90/linaro/bin/
+#CUR_TC=/home/wzhy90/android-toolchain-eabi-linaro-4.6-2011.10/bin/
+## GCC 4.7 Linaro
+#CUR_TC=/home/wzhy90/android-toolchain-eabi-linaro-4.7-2013.01/bin/
+## GCC 4.9 Linaro
 CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1/bin/
 
 ### compile kernel
--- menu_4.9.sh
+++ menu_4.9.sh
@@ -7,7 +7,7 @@
 ## GCC 4.9 Linaro
 CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1/bin/
 
-### cleaning
-#ARCH=arm CROSS_COMPILE=${CUR_TC}arm-eabi- make distclean
-ARCH=arm CROSS_COMPILE=${CUR_TC}arm-gnueabi- make distclean
+### compile kernel
+#ARCH=arm CROSS_COMPILE=${CUR_TC}arm-eabi- make menuconfig
+ARCH=arm CROSS_COMPILE=${CUR_TC}arm-gnueabi- make menuconfig
 
