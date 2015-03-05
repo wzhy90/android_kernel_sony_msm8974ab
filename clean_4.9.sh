f639777c93e72efcdad20abc516498804a77b969
wzhy90 <wzhy90@gmail.com>
2015-03-05 09:16:52 +0800

    Scripts switch to gcc linaro 4.9.3

--- clean_4.9.sh
+++ clean_4.9.sh
@@ -5,9 +5,9 @@
 ## GCC 4.7 Linaro
 #CUR_TC=/home/wzhy90/android-toolchain-eabi-linaro-4.7-2013.01/bin/
 ## GCC 4.9 Linaro
-CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1/bin/
+CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.02/bin/
 
 ### cleaning
-#ARCH=arm CROSS_COMPILE=${CUR_TC}arm-eabi- make distclean
-ARCH=arm CROSS_COMPILE=${CUR_TC}arm-gnueabi- make distclean
+ARCH=arm CROSS_COMPILE=${CUR_TC}arm-eabi- make distclean
+#ARCH=arm CROSS_COMPILE=${CUR_TC}arm-gnueabi- make distclean
 
--- menu_4.9.sh
+++ menu_4.9.sh
@@ -5,9 +5,9 @@
 ## GCC 4.7 Linaro
 #CUR_TC=/home/wzhy90/android-toolchain-eabi-linaro-4.7-2013.01/bin/
 ## GCC 4.9 Linaro
-CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1/bin/
+CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.02/bin/
 
 ### compile kernel
-#ARCH=arm CROSS_COMPILE=${CUR_TC}arm-eabi- make menuconfig
-ARCH=arm CROSS_COMPILE=${CUR_TC}arm-gnueabi- make menuconfig
+ARCH=arm CROSS_COMPILE=${CUR_TC}arm-eabi- make menuconfig
+#ARCH=arm CROSS_COMPILE=${CUR_TC}arm-gnueabi- make menuconfig
 
