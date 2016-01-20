c0218b245067ccdab5c472f81a19a1bbc07f0b9f
wzhy90 <wzhy90@gmail.com>
2016-01-20 22:49:38 +0800

    Update script.

--- build_4.9.sh
+++ build_4.9.sh
@@ -5,11 +5,16 @@
 ## GCC 4.7 Linaro
 #CUR_TC=/home/wzhy90/android-toolchain-eabi-linaro-4.7-2013.01/bin/
 ## GCC 4.9 Linaro
-CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1/bin/
+#CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1/bin/
+## GCC 4.9 SaberMod
+#CUR_TC=/home/wzhy90/toolchain-arm_sabermod-gcc-4.9/bin/
+## GCC 4.8 Uber
+CUR_TC=/home/wzhy90/toolchain-arm_uber-gcc-4.9/bin/
+
 
 ### compile kernel
-#ARCH=arm CROSS_COMPILE=${CUR_TC}arm-eabi- make -j4
-ARCH=arm CROSS_COMPILE=${CUR_TC}arm-gnueabi- make -j4
+ARCH=arm CROSS_COMPILE=${CUR_TC}arm-eabi- make -j4
+#ARCH=arm CROSS_COMPILE=${CUR_TC}arm-gnueabi- make -j4
 
 if [ "$?" != 0 ]
 then
