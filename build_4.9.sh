ea99d4aa9a0be994e5085078546a715461bac2df
wzhy90 <wzhy90@gmail.com>
2016-05-26 18:36:00 +0800

    Change toolchain to linaro 4.9.4.

--- build_4.9.sh
+++ build_4.9.sh
@@ -5,11 +5,11 @@
 ## GCC 4.7 Linaro
 #CUR_TC=/home/wzhy90/android-toolchain-eabi-linaro-4.7-2013.01/bin/
 ## GCC 4.9 Linaro
-#CUR_TC=/home/wzhy90/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1/bin/
+CUR_TC=/home/wzhy90/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/
 ## GCC 4.9 SaberMod
 #CUR_TC=/home/wzhy90/toolchain-arm_sabermod-gcc-4.9/bin/
 ## GCC 4.8 Uber
-CUR_TC=/home/wzhy90/toolchain-arm_uber-gcc-4.9/bin/
+#CUR_TC=/home/wzhy90/toolchain-arm_uber-gcc-4.9/bin/
 
 
 ### compile kernel
